476095dc41d5231800053a724365e433229726db
Luis <lgmarchioreh@gmail.com>
2023-06-18 20:45:39 -0300

    primeira tentativa

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,126 @@
 /* Desenvolva sua lógica aqui ... */
+var numberfilter = 0;
+
+function createCard(product){
+    const card = document.createElement("li");
+    const cardImg = document.createElement("img");
+    const cardText = document.createElement("p");
+    const cardTitle = document.createElement("h2");
+    const cardButtonContainer = document.createElement("div");
+    const cardSpan = document.createElement("span");
+    const cardPrice = document.createElement("p");
+    const cardButton = document.createElement("button");
+
+    card.className = "card__container";
+    card.id = `disc-${product.id}`
+    cardImg.className = "card__image";
+    cardText.className = "card__text";
+    cardTitle.className = "card__title";
+    cardButtonContainer.className = "card__button-container";
+    cardSpan.classList = "card__span";
+    cardPrice.classList = "card__price";
+    cardButton.classList = "card__button";
+    cardButton.id = product.id;
+
+    cardImg.src = product.img;
+    cardImg.alt = product.title;
+    cardText.innerText = `${product.band} ${product.year}`
+    cardTitle.innerText = product.title;
+    cardSpan.innerText = "R$ ";
+    cardPrice.innerText = product.price;
+    cardButton.innerText = "Comprar"
+
+    card.appendChild(cardImg);
+    card.appendChild(cardText);
+    card.appendChild(cardTitle);
+    cardSpan.appendChild(cardPrice);
+    cardButtonContainer.appendChild(cardSpan);
+    cardButtonContainer.appendChild(cardButton);
+    card.appendChild(cardButtonContainer);
+
+    return card;
+}
+
+function createButton(button, id) {
+    const buttonContainer = document.createElement("li");
+    const buttonButton = document.createElement("button");
+  
+    buttonContainer.className = "button__container";
+    buttonContainer.id = `item-${id}`;
+    buttonButton.className = "button__button";
+    buttonButton.id = id;
+  
+    buttonButton.addEventListener("click", function() {
+      filter(id, products);
+    });
+  
+    buttonButton.innerText = button;
+  
+    buttonContainer.appendChild(buttonButton);
+    return buttonContainer;
+  }
+  
+
+function rederCards(products){
+    const discsContainer = document.querySelector(".discs__container");
+
+
+    products.forEach(product =>{
+        const card = createCard(product);
+        discsContainer.appendChild(card);
+    });
+}
+
+function renderButtons(array){
+    const buttonContainer = document.querySelector(".buttons__container");
+    var idCat = 0
+
+    array.forEach(buttonText => { // Renomear o parâmetro para buttonText
+        const buttonItem = createButton(buttonText, idCat); // Renomear a variável para buttonItem
+        idCat++;
+        buttonContainer.appendChild(buttonItem); // Renomear a variável para buttonItem
+    })
+}
+
+function filter(number, array) {
+    const discsContainer = document.querySelector(".discs__container");
+    discsContainer.innerHTML = "";
+    numberfilter = number;
+    console.log(numberfilter);
+    
+    array.forEach(item => {
+      if (item.category === number || number === 0) {
+        const card = createCard(item);
+        discsContainer.appendChild(card);
+      }
+    });
+  }
+  
+  function filterRange(array) {
+    const priceRange = document.querySelector(".price__range");
+    const discsContainer = document.querySelector(".discs__container");
+    const priceSpan = document.querySelector(".price__span");
+  
+    priceRange.addEventListener("input", () => {
+      priceSpan.innerHTML = "";
+      discsContainer.innerHTML = "";
+      console.log(numberfilter);
+  
+      priceSpan.innerText = priceRange.value;
+  
+      array.forEach(item => {
+        if (item.price <= priceRange.value) {
+          if (item.category === numberfilter || numberfilter === 0) {
+            const card = createCard(item);
+            discsContainer.appendChild(card);
+          }
+        }
+      });
+    });
+  }
+
+toggleDarkMode();
+filterRange(products);
+rederCards(products);
+renderButtons(categories);
+themePreferenceAnalysis();
--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,43 @@
-/* Desenvolva sua lógica aqui ... */
+var dark = false;
+
+function toggleDarkMode() {
+    const button = document.querySelector(".header__image");
+    if (dark == false) {
+        button.src = "./src/assets/img/moon.svg";
+    } else if(dark){
+        button.src = "./src/assets/img/sun.svg";
+    }
+
+}
+
+function changeTheme(){
+    const body = document.querySelector("body");
+    const buttons = document.querySelectorAll("button");
+    const titles1 = document.querySelectorAll("h1");
+    const titles2 = document.querySelectorAll("h2");
+    const paragraphs = document.querySelectorAll("p");
+    const spans = document.querySelectorAll("span");
+    const cardContainers = document.querySelectorAll(".card__container");
+    const cardText = document.querySelector(".card__text");
+    const theme = "darkMode";
+    dark = !dark;
+
+    body.classList.toggle(theme);
+    buttons.forEach((button) => button.classList.toggle(theme));
+    titles1.forEach((title) => title.classList.toggle(theme));
+    titles2.forEach((title) => title.classList.toggle(theme));
+    paragraphs.forEach((paragraph) => paragraph.classList.toggle(theme));
+    spans.forEach((span) => span.classList.toggle(theme));
+    cardContainers.forEach((cardContainer) => cardContainer.classList.toggle(theme));
+    cardText.classList.toggle(theme);
+    localStorage.setItem(theme, JSON.stringify(dark));
+}
+
+function themePreferenceAnalysis() {
+    darkMode = JSON.parse(localStorage.getItem("darkMode"));
+    console.log(darkMode);
+    if (darkMode) {
+        changeTheme()
+    }
+  }
+  
